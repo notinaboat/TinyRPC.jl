bcbd7440de631c07a5526bae8de2dbb886bea3bb
notinaboat <notinaboat@icloud.com>
2021-09-13 08:43:50 +1000

    add automatic timestamp to ChannelLogger

--- src/ChannelLogger.jl
+++ src/ChannelLogger.jl
@@ -2,7 +2,8 @@ using Logging
 
 struct ChannelLogger <: AbstractLogger
     c::Channel{Tuple}
-    ChannelLogger() = new(Channel{Tuple}(1000))
+    timestamp::Bool
+    ChannelLogger(;timestamp=true) = new(Channel{Tuple}(1000), timestamp)
 end
 
 const collect_max = 10
@@ -26,9 +27,12 @@ Logging.shouldlog(::ChannelLogger, args...) = true
 Logging.min_enabled_level(::ChannelLogger) = Logging.Debug
 Logging.catch_exceptions(::ChannelLogger) = false
 
+log_squash(v::Union{Integer,Float64,Bool}) = v
+log_squash(v) = string(v)
+
 function Logging.handle_message(l::ChannelLogger,
                                 level, message, _module, group, id, file, line;
-                                kw...) 
+                                timestamp=nothing, kw...) 
 
     # To avoid feedback loops,
     # don't put TinyRPC Debug logs in the remote logging channel
@@ -41,6 +45,18 @@ function Logging.handle_message(l::ChannelLogger,
         take!(l.c)
     end
 
+    # Convert the kw args into a Vector.
+    kwv = Pair{Symbol,Any}[Symbol(k) => log_squash(v) for (k, v) in kw]
+
+    # Add a timestamp.
+    if l.timestamp && timestamp == nothing
+        timestamp == time()
+    end
+    if timestamp != nothing
+        push!(kwv, :timestamp => timestamp)
+    end
+
+
     # Convert args to basic types.
     put!(l.c, ((LogLevel(level),
                 string(message),
@@ -49,5 +65,5 @@ function Logging.handle_message(l::ChannelLogger,
                 Symbol(id),
                 string(file),
                 Int32(line)),
-                [Symbol(k) => string(v) for (k, v) in kw]))
+                kwv))
 end
